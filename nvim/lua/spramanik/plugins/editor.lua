a9183ee6b570fc2a7125de307491b17212357857
sourabpramanik <shubpramanik241@gmail.com>
2024-03-26 08:01:23 +0530

    fix: treesitter config

--- nvim/lua/spramanik/plugins/editor.lua
+++ nvim/lua/spramanik/plugins/editor.lua
@@ -1,5 +1,32 @@
 return{
   {
+    "laytan/cloak.nvim",
+    config = function()
+      require('cloak').setup({
+        enabled = true,
+        cloak_character = '*',
+        -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+        highlight_group = 'Comment',
+        -- Set to true to cloak Telescope preview buffers. (Required feature not in 0.1.x)
+        cloak_telescope = true,
+        patterns = {
+          {
+            -- Match any file starting with '.env'.
+            -- This can be a table to match multiple file patterns.
+            file_pattern = {
+              '.env*',
+              '.dev.vars'
+            },
+            -- Match an equals sign and any character after it.
+            -- This can also be a table of patterns to cloak,
+            -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
+            cloak_pattern = '=.+',
+          },
+        },
+      })
+    end
+  },
+  {
 		"dinhhuy258/git.nvim",
 		event = "BufReadPre",
 		opts = {
@@ -151,7 +178,7 @@ return{
 			}
 			opts.extensions = {
 				file_browser = {
-					theme = "dropdown",
+					theme = "ivy",
 					-- disables netrw and use telescope-file-browser in its place
 					hijack_netrw = true,
 					mappings = {
